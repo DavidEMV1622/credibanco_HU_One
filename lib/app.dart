0097430329cb062cd9ca323a3cd2258eb53215a8
david <davidemv1622@gmail.com>
2023-08-04 16:47:28 -0500

    pantallas sin funcionalidad completas y formato de letra

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,4 @@
 import 'package:credibanco_hu_aca_1715/presentation/screens/avisos_page.dart';
-import 'package:credibanco_hu_aca_1715/presentation/widgets/listView_prueba.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
